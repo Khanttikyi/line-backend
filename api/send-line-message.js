e7fb06baaf5c1dd5ee3eabe5a3b0e10edf814e37
Khant Ti Kyi <khant@blue-stone.net>
2025-02-23 16:05:13 +0700

    chnage

--- api/send-line-message.js
+++ api/send-line-message.js
@@ -1,11 +1,18 @@
+
+
 const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
 
 const app = express();
 app.use(express.json());
-app.use(cors());
 
+// Allow CORS for all origins (you can specify allowed origins)
+app.use(cors({
+    origin: "*", // Allow all domains
+    methods: "GET, POST, PUT, DELETE, OPTIONS", // Allow all methods
+    allowedHeaders: "*", // Allow all headers
+}));
 const LINE_API_URL = "https://api.line.me/v2/bot/message/push";
 const CHANNEL_ACCESS_TOKEN = 'DQ+6KCdWi+hgRn4h5CPRYB+6Eb41FANRufSvIg6ljaH/pdcgsKm65CcXg226Vlacbqv+qo/pkPlWmo1i1qoOkPoGuO+9PnF580V08OfFTWwf/pf1OmKd5e3hLRkaltY1ZxHzefci4/B3zTo+tdhFvwdB04t89/1O/w1cDnyilFU=';
 
@@ -27,7 +34,7 @@ app.post("/api/send-line-message", async (req, res) => {
             }
         );
 
-        res.json({ success: true, data: response.data });
+        res.status(200).json({ success: true, data: response.data });
     } catch (error) {
         console.error("Error sending LINE message:", error.response?.data || error.message);
         res.status(500).json({ success: false, error: error.response?.data || error.message });
